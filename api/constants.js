9077bcc016358fbf7ad0e8c0e80aa67cc82ceb2b
bkominek <brandon.kominek@gmail.com>
2017-11-13 16:01:38 -0500

    add audio files, play using node

---
+++ api/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  COLIN: 'smrt.m4a',
+  MAV: 'danger_zone.m4a',
+  DEFAULT: 'ding.mp3'
+}
--- api/controllers/salesBellController.js
+++ api/controllers/salesBellController.js
@@ -1,5 +1,13 @@
 'use strict';
+var player = require('play-sound')()
+var path = require('path');
+const files = require('../constants');
 
 exports.play_sound = function(req, res) {
+  var appDir = path.dirname(require.main.filename);
+  player.play(appDir + '/assets/' + files.COLIN, function(err){
+    if (err) throw err
+  })
+
   res.json({message: "cool beans yo"});
 };
