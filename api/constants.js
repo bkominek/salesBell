c7c25703f25856d39f68abe2ccb9c7b8b3c9e129
bkominek <brandon.kominek@gmail.com>
2017-11-14 12:27:34 -0500

    slack usernames

--- api/constants.js
+++ api/constants.js
@@ -1,10 +1,10 @@
 module.exports = {
   BRANDON: 'meow.wav',
-  CARMINE: 'italian.m4a',
+  CARMCERRONE: 'italian.m4a',
   COLIN: 'smrt.m4a',
   MAV: 'danger_zone.m4a',
-  PAUL: 'ignorant.m4a',
-  SEAN: 'squat.m4a',
-  TARA: 'coffees_for_closers.m4a',
+  PAULNOWOSIELSKI: 'ignorant.m4a',
+  SEAN_ERJAVEC: 'squat.m4a',
+  TARACYBULSKI: 'coffees_for_closers.m4a',
   DEFAULT: 'ding.mp3'
 }
