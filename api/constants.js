45cf396bd11f2cf2195f7c7abd0b408da53273dc
bkominek <brandon.kominek@gmail.com>
2017-11-14 12:05:50 -0500

    add audio files

--- api/constants.js
+++ api/constants.js
@@ -1,5 +1,9 @@
 module.exports = {
+  CARMINE: 'italian.m4a',
   COLIN: 'smrt.m4a',
   MAV: 'danger_zone.m4a',
+  PAUL: 'ignorant.m4a',
+  SEAN: 'squat.m4a',
+  TARA: 'coffees_for_closers.m4a',
   DEFAULT: 'ding.mp3'
 }
--- api/controllers/salesBellController.js
+++ api/controllers/salesBellController.js
@@ -4,6 +4,7 @@ var path = require('path');
 const files = require('../constants');
 
 exports.play_sound = function(req, res) {
+  console.log(req);
   var appDir = path.dirname(require.main.filename);
   player.play(appDir + '/assets/' + files.COLIN, function(err){
     if (err) throw err
