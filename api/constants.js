a6ca015d4e37d1b20bdae7da34b2a7969de6ea89
bkominek <brandon.kominek@gmail.com>
2017-11-15 14:32:37 -0500

    add troy, remove me

--- api/constants.js
+++ api/constants.js
@@ -1,11 +1,11 @@
 module.exports = {
-  BRANDON: 'meow.wav',
   CARMCERRONE: 'italian.m4a',
   COLIN: 'smrt.m4a',
   MAV: 'danger_zone.m4a',
   PAULNOWOSIELSKI: 'ignorant.m4a',
   SEAN_ERJAVEC: 'squat.m4a',
   TARACYBULSKI: 'coffees_for_closers.m4a',
+  TROY: 'woo.mp3',
   GONG: 'gong.wav',
   DEFAULT: 'ding.mp3'
 }
