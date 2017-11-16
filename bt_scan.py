ea8f935a808e7917909b620722a99c7eb0006d28
bkominek <brandon.kominek@gmail.com>
2017-11-16 11:35:05 -0500

    add pyglet

--- bt_scan.py
+++ bt_scan.py
@@ -4,14 +4,18 @@ print "performing inquiry..."
 
 target_devices = ["Nexus 6P"]
 found_devices = []
-
 nearby_devices = discover_devices(lookup_names = True)
-print nearby_devices
+
 print "found %d devices" % len(nearby_devices)
 
-for name, addr in nearby_devices:
-  if name in target_devices:
+for addr, name in nearby_devices:
+  if name in target_devices and name not in found_devices:
     found_devices.append(name)
   print " %s - %s" % (addr, name)
 
 print found_devices
+
+music = pyglet.resource.media('assets/woo.mp3')
+music.play()
+
+pyglet.app.run()
