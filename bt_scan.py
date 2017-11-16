96617beeba654bf1185103f1e4616857f26a3e3a
bkominek <brandon.kominek@gmail.com>
2017-11-16 11:18:15 -0500

    debug

--- bt_scan.py
+++ bt_scan.py
@@ -2,9 +2,16 @@ from bluetooth import *
 
 print "performing inquiry..."
 
+target_devices = ["Nexus 6P"]
+found_devices = []
+
 nearby_devices = discover_devices(lookup_names = True)
 
 print "found %d devices" % len(nearby_devices)
 
 for name, addr in nearby_devices:
+  if name in target_devices:
+    found_devices.append(name)
   print " %s - %s" % (addr, name)
+
+print found_devices
