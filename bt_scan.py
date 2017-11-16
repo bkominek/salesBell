5fc7347c81db321ab4e3dde259233168f3fec468
bkominek <brandon.kominek@gmail.com>
2017-11-16 14:02:41 -0500

    while loop

--- bt_scan.py
+++ bt_scan.py
@@ -1,6 +1,7 @@
 from bluetooth import *
 import pygame
 import time
+import datetime
 
 pygame.init()
 music_list = {'Nexus 6P': {'file': 'assets/woo.mp3', 'duration': 3}}
@@ -9,8 +10,9 @@ print "scanning for bluetooth devices..."
 
 target_devices = ["Nexus 6P"]
 found_devices = []
+search = True
 
-while True:
+while search:
   nearby_devices = discover_devices(lookup_names = True)
 
   for addr, name in nearby_devices:
@@ -21,3 +23,7 @@ while True:
       pygame.mixer.music.load(music_list[name]['file'])
       pygame.mixer.music.play()
       time.sleep(music_list[name]['duration'])
+
+  now = datetime.datetime.now()
+  if now.hour > 10 and now.minute > 30
+    search = False
