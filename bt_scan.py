cb2aafe8072f26198c376aa386c2a05564bbf498
bkominek <brandon.kominek@gmail.com>
2017-11-16 11:20:30 -0500

    debug

--- bt_scan.py
+++ bt_scan.py
@@ -6,7 +6,7 @@ target_devices = ["Nexus 6P"]
 found_devices = []
 
 nearby_devices = discover_devices(lookup_names = True)
-
+print nearby_devices
 print "found %d devices" % len(nearby_devices)
 
 for name, addr in nearby_devices:
